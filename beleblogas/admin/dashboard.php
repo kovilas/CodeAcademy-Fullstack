fdc2af5a99561dbe46c0cf1318e5c0219fce19fa
Kovilas <povilas.klusas@gmail.com>
2020-05-04 18:42:26 +0300

    beleblogas

---
+++ beleblogas/admin/dashboard.php
@@ -0,0 +1,43 @@
+<?php  include('../config.php'); ?>
+	<?php include(ROOT_PATH . '/admin/includes/admin_functions.php'); ?>
+	<?php include(ROOT_PATH . '/admin/includes/head_section.php'); ?>
+	<title>Admin | Dashboard</title>
+</head>
+<body>
+	<div class="header">
+		<div class="logo">
+			<a href="<?php echo BASE_URL .'admin/dashboard.php' ?>">
+				<h1>beleBlogas - Admin</h1>
+			</a>
+		</div>
+		<?php if (isset($_SESSION['user'])): ?>
+			<div class="user-info">
+				<span><?php echo $_SESSION['user']['username'] ?></span> &nbsp; &nbsp; 
+				<a href="<?php echo BASE_URL . '/logout.php'; ?>" class="logout-btn">logout</a>
+			</div>
+		<?php endif ?>
+	</div>
+	<div class="container dashboard">
+		<h1>Welcome</h1>
+		<div class="stats">
+			<a href="users.php" class="first">
+				<span>4</span> <br>
+				<span>Newly registered users (nepadarytas)</span>
+			</a>
+			<a href="posts.php">
+				<span>42</span> <br>
+				<span>Published posts (nepadarytas)</span>
+			</a>
+			<a>
+				<span>420</span> <br>
+				<span>Published comments (nepadarytas)</span>
+			</a>
+		</div>
+		<br><br><br>
+		<div class="buttons">
+			<a href="users.php">Add Users</a>
+			<a href="posts.php">Add Posts</a>
+		</div>
+	</div>
+</body>
+</html>
---
+++ beleblogas/includes/footer.php
@@ -0,0 +1,9 @@
+		<div class="footer">
+			<p><?php echo date('Y'); ?></p>
+		</div>
+		<!-- // footer -->
+
+	</div>
+	<!-- // container -->
+</body>
+</html>
